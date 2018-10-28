4ef3c61cd7c31bf7f9216033ce8ff4a342562e9a
Daan Thijs <daan.thijs1@student.kuleuven.be>
2018-10-28 15:52:57 +0100

    renamed map, added steepest_descent class+runner

---
+++ locsearch/localsearch/steepestdescent/steepest_descent.py
@@ -0,0 +1,16 @@
+from locsearch.localsearch.abstract_local_search import AbstractLocalSearch
+
+
+class SteepestDescent(AbstractLocalSearch):
+    """Implementation of a steepest descent algorithm.descent
+
+    """
+
+    def __init__(self):
+        super().__init__()
+
+    def run():
+        # do stuff
+        pass
+
+
---
+++ locsearch/runner/steepest_descent_runner.py
@@ -0,0 +1,30 @@
+from locsearch.runner.abstract_runner import AbstractRunner
+from locsearch.localsearch.steepestdescent.steepest_descent import SteepestDescent
+
+
+class SteepestDescentRunner(AbstractRunner):
+    """runner for SteepestDescent class"""
+
+    def __init__(self):
+        super().__init__()
+
+    def run():
+        """initializes and runs an instance of the SteepestDescent class
+
+        """
+
+        # read data
+
+        # create evaluation function
+
+        # create move
+
+        # create and initialize solution
+
+        # create instance of localsearch algorithm and set solution
+
+        # get results from localsearch algorithm
+
+        # output
+
+        pass  # this pass is useless, delete when implemented.
