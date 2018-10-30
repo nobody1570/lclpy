925d675639f6e0ecc6ff548ac38b19ed844b7955
Daan Thijs <daan.thijs1@student.kuleuven.be>
2018-10-30 14:59:28 +0100

    Fixed doctest.

--- locsearch/termination/must_improve_termination_criterion.py
+++ locsearch/termination/must_improve_termination_criterion.py
@@ -13,6 +13,24 @@ class MustImproveTerminationCriterion(AbstractTerminationCriterion):
     Examples
     --------
 
+    5 iterations with improvement. After that there are no more
+    improvements.The dataset eval_values is hardcoded:
+
+    .. doctest::
+
+            >>> import numpy
+            >>> from locsearch.termination.must_improve_termination_criterion import MustImproveTerminationCriterion
+            >>> eval_values = numpy.array([0, 2, 14, 15, 20, 3, 3, 4, 2, 1, 0, 12])
+            >>> index = 0
+            >>> test = MustImproveTerminationCriterion()
+            >>> while test.keep_running():
+            ...     pass # code to execute
+            ...     test.check_new_value(eval_values[index])
+            ...     pass # more code to execute
+            ...     index += 1 # counting iterations + index
+            >>> index # Amount of iterations.
+            6
+
 
 
     """
@@ -43,5 +61,7 @@ class MustImproveTerminationCriterion(AbstractTerminationCriterion):
             Is the new evaluation value of a solution. Value must be positive.
 
         """
-        if value < self._old_best_value:
+        if value > self._old_best_value:
+            self._old_best_value = value
+        else:
             self.run = False
