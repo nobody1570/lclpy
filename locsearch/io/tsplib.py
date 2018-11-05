475ddc37b04eba99956f7fcc4c8ab2bfa9f65f71
Daan Thijs <daan.thijs1@student.kuleuven.be>
2018-11-05 11:34:45 +0100

    Added docstrings to tsplib + added namedtuples

--- locsearch/io/tsplib.py
+++ locsearch/io/tsplib.py
@@ -1,23 +1,111 @@
 import string
 import numpy
 import math
+import collections
 
 
 def _euclidian(from_x, from_y, to_x, to_y):
+    """Calculates the euclidian distance between 2 points in 2D space.
+
+    Parameters
+    ----------
+    from_x : int or float
+        The x coordinate of the 1st point.
+    from_y : int or float
+        The y coordinate of the 1st point.
+    to_x : int or float
+        The x coordinate of the 2nd point.
+    to_y : int or float
+        The y coordinate of the 2nd point.
+
+    Returns
+    -------
+    float
+        The euclidian distance between the 2 points.
+
+
+    """
     return math.sqrt((to_x - from_x)**2 + (to_y - from_y)**2)
 
 
 def _euclidian_rounded_up(from_x, from_y, to_x, to_y):
     return int(math.ceil(math.sqrt((to_x - from_x)**2 + (to_y - from_y)**2)))
+    """Calculates the euclidian distance between 2 points in 2D space.
+
+    Parameters
+    ----------
+    from_x : int or float
+        The x coordinate of the 1st point.
+    from_y : int or float
+        The y coordinate of the 1st point.
+    to_x : int or float
+        The x coordinate of the 2nd point.
+    to_y : int or float
+        The y coordinate of the 2nd point.
+
+    Returns
+    -------
+    int
+        The euclidian distance between the 2 points, rounded up to the nearest
+        int.
+
+
+    """
 
 
 def _manhattan(from_x, from_y, to_x, to_y):
+    """Calculates the manhattan distance between 2 points in 2D space.
+
+    Parameters
+    ----------
+    from_x : int or float
+        The x coordinate of the 1st point.
+    from_y : int or float
+        The y coordinate of the 1st point.
+    to_x : int or float
+        The x coordinate of the 2nd point.
+    to_y : int or float
+        The y coordinate of the 2nd point.
+
+    Returns
+    -------
+    int or float
+        The manhattan distance between the 2 points.
+
+
+    """
     return abs(to_x - from_x) + abs(to_y - from_y)
 
 
 def _default_processing(data, dist_func, type=numpy.float_):
+    """Creates a dict and calculates the distance matrix for a 2D tsp problem.
 
-    # make dict
+    Parameters
+    ----------
+    data : list of string
+        The strings in the list always contain the name of a point,
+        the x coordinate and the y coordinate in that order seperated by
+        spaces.
+    dist_func : function
+        A function to calculate the distance between the points. This
+        function's arguments must be the x and y coordinates of the first
+        point, followed by the x and y coordinates of the second point.
+    type : numpy.dtype, optional
+        The data type used by the numpy array. The default is numpy.float_,
+        which is the default datatype when creating a numpy.ndarray.
+
+    Returns
+    -------
+    dist_matrix : numpy.ndarray
+        The distance matrix for the problem.
+    dictionary : {int : int}
+        A dictionary that can convert a position the distance matrix to the
+        name given in data.
+
+
+    """
+
+    # make dictionary
     dictionary = {}
 
     position = 0
@@ -47,10 +135,40 @@ def _default_processing(data, dist_func, type=numpy.float_):
             dist_matrix[i][j] = dist_func(
                 i_dist_x, i_dist_y, j_dist_x, j_dist_y)
 
-    return (dist_matrix, dictionary)
+    DefaultProcessingResults = collections.namedtuple(
+        'DefaultProcessingResults', ['distance_matrix', 'dictionary'])
+
+    return DefaultProcessingResults(dist_matrix, dictionary)
 
 
 def read_tsplib(filename):
+    """Converts tsplib file format to useable data structures.
+
+    Parameters
+    ----------
+    filename : str
+        absolute or relative path to the file that contains the data.
+
+    Returns
+    -------
+    distance_matrix : numpy.ndarray
+        The distance matrix for the problem.
+    dictionary : {int : int}
+        A dictionary that can convert a position the distance matrix to the
+        name given in data.
+    metadata : list of str
+        Contains the metadata of the problem.
+
+    Examples
+    --------
+    Read "testfile.tsp" from the map "data" in the current working directory:
+
+    .. code-block:: python
+
+        from locsearch.io.tsplib import read_tsplib
+        read_tsplib('data/testfile.tsp')
+
+    """
 
     metadata = []
     data = []
@@ -93,4 +211,6 @@ def read_tsplib(filename):
     else:
         (dist_matrix, dictionary) = solve(data, dist_func, dtype)
 
-    return (dist_matrix, dictionary, metadata)
+    TsplibData = collections.namedtuple(
+        'TsplibData', ['distance_matrix', 'dictionary', 'metadata'])
+    return TsplibData(dist_matrix, dictionary, metadata)
