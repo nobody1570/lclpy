f9cbf6432edd536ac845153e3022909006807758
Daan Thijs <daan.thijs1@student.kuleuven.be>
2018-11-20 14:58:18 +0100

    Minor fix in docstring abstr local search.

--- locsearch/localsearch/abstract_local_search.py
+++ locsearch/localsearch/abstract_local_search.py
@@ -6,20 +6,10 @@ class AbstractLocalSearch(ABC):
 
     This class should only contain the logic needed to run the local search.
     I/O and plotting should be handled by a runner.
-    The localsearch should contain a working terminationCriterion.
+
     """
 
     def __init__(self):
-        """__init__
-
-        Parameters
-        ----------
-        solution : AbstractLocalSearchSolution
-            An object derived from AbstractLocalSearchSolution that contains
-            the data needed to perform the local search.
-
-        """
-
         super().__init__()
 
     @abstractmethod
