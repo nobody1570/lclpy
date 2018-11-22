3ed9a6751e7c86894d873f78201d1524d3479a27
Daan Thijs <daan.thijs1@student.kuleuven.be>
2018-11-22 11:21:46 +0100

    Docstring AbstractLocalSearch.

--- locsearch/localsearch/abstract_local_search.py
+++ locsearch/localsearch/abstract_local_search.py
@@ -2,10 +2,10 @@ from abc import ABC, abstractmethod
 
 
 class AbstractLocalSearch(ABC):
-    """Template for the implementation of local search algorithms
+    """Template for the implementation of local search algorithms.
 
     This class should only contain the logic needed to run the local search.
-    I/O and plotting should be handled by a runner.
+    I/O and plotting should be handled by an AbstractRunner.
 
     """
 
@@ -18,7 +18,7 @@ class AbstractLocalSearch(ABC):
 
         Returns
         -------
-        Results : collections.namedtuple
+        collections.namedtuple
             Contains the best found case and the evaluation value for said
             case.
 
