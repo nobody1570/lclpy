a4c2eed69884f18c6381e1c97d7c5a9ce9ee5290
Daan Thijs <daan.thijs1@student.kuleuven.be>
2019-01-10 09:32:25 +0100

    Doctest simulated annealing fixed.

--- locsearch/localsearch/simulatedannealing/simulated_annealing.py
+++ locsearch/localsearch/simulatedannealing/simulated_annealing.py
@@ -2,6 +2,8 @@ from locsearch.localsearch.abstract_local_search import AbstractLocalSearch
 from locsearch.localsearch.acceptance.simulated_annealing_acceptance_function \
     import SimulatedAnnealingAcceptanceFunction
 from collections import namedtuple
+from locsearch.aidfunc.is_improvement_func \
+    import bigger, bigger_or_equal, smaller, smaller_or_equal
 
 
 class SimulatedAnnealing(AbstractLocalSearch):
@@ -37,10 +39,15 @@ class SimulatedAnnealing(AbstractLocalSearch):
         The current "temperature".
     _acceptance_function : SimulatedAnnealingAcceptanceFunction
         The acceptance function used.
+    _is_improvement
+        Function used to determine if a certain delta value is an improvement
+        or equal to the current value.
+    _is_better
+        Function used to determine if a certain value is an improvement.
 
     Examples
     --------
-    A simple example:
+    Minimising example:
 
     .. doctest::
 
@@ -86,11 +93,58 @@ class SimulatedAnnealing(AbstractLocalSearch):
         >>> algorithm.run()
         Results(best_order=array([0, 2, 3, 1]), best_value=15)
 
+    Maximising example, note that the distance matrix is different:
+
+    .. doctest::
+
+        >>> import numpy
+        >>> import random
+        >>> from locsearch.localsearch.move.tsp_array_swap import TspArraySwap
+        >>> from locsearch.localsearch.simulatedannealing.simulated_annealing \\
+        ...     import SimulatedAnnealing
+        >>> from locsearch.localsearch.simulatedannealing.geometric_cooling_function \\
+        ...     import GeometricCoolingFunction
+        >>> from locsearch.localsearch.simulatedannealing.cnst_iterations_temp_function \\
+        ...     import CnstIterationsTempFunction
+        >>> from locsearch.evaluation.tsp_evaluation_function \\
+        ...     import TspEvaluationFunction
+        >>> from locsearch.termination.min_temperature_termination_criterion \\
+        ...     import MinTemperatureTerminationCriterion
+        >>> from locsearch.solution.tsp_solution import TspSolution
+        ... # seed random
+        ... # (used here to always get the same output, this obviously is not
+        ... #                                  needed in your implementation.)
+        >>> random.seed(0)
+        ... # init solution
+        >>> distance_matrix = numpy.array(
+        ... [[0, 8, 5, 2],
+        ...  [8, 0, 4, 7],
+        ...  [5, 4, 0, 1],
+        ...  [2, 7, 1, 0]])
+        >>> size = distance_matrix.shape[0]
+        >>> move = TspArraySwap(size)
+        >>> evaluation = TspEvaluationFunction(distance_matrix, move)
+        >>> solution = TspSolution(evaluation, move, size)
+        ... # init termination criterion
+        >>> termination_criterion = MinTemperatureTerminationCriterion()
+        ... # init cooling function
+        >>> cooling_func = GeometricCoolingFunction()
+        ... # init CnstIterationsTempFunction
+        ... # (determines amount of itertions in function of the temperature)
+        >>> i_for_temp = CnstIterationsTempFunction()
+        ... # init SimulatedAnnealing
+        >>> algorithm = SimulatedAnnealing(
+        ...     solution, termination_criterion,
+        ...     cooling_func, i_for_temp, minimise=False)
+        ... # run algorithm
+        >>> algorithm.run()
+        Results(best_order=array([0, 2, 3, 1]), best_value=21)
+
     """
 
     def __init__(self, solution, termination_criterion,
                  cooling_function, iterations_for_temp_f,
-                 start_temperature=2000):
+                 start_temperature=2000, minimise=True):
         super().__init__()
 
         self._solution = solution
@@ -99,6 +153,16 @@ class SimulatedAnnealing(AbstractLocalSearch):
         self._iterations_for_temp_f = iterations_for_temp_f
         self._temperature = start_temperature
         self._acceptance_function = SimulatedAnnealingAcceptanceFunction()
+
+        if minimise:
+            self._is_improvement = smaller_or_equal
+            self._is_better = smaller
+            self._acceptance_function = SimulatedAnnealingAcceptanceFunction()
+        else:
+            self._is_improvement = bigger_or_equal
+            self._is_better = bigger
+            self._acceptance_function = \
+                SimulatedAnnealingAcceptanceFunction(diff_multiplier=-1)
 
     def run(self):
         """Starts running the simulated annealing algorithm.
@@ -118,6 +182,8 @@ class SimulatedAnnealing(AbstractLocalSearch):
         base_value = self._solution.evaluate()
         self._solution.set_as_best(base_value)
 
+        # init terminitaion criterion
+        self._termination_criterion.check_new_value(base_value)
         self._termination_criterion.start_timing()
 
         # main loop
@@ -138,14 +204,15 @@ class SimulatedAnnealing(AbstractLocalSearch):
                 delta = self._solution.evaluate_move(move)
 
                 # accept or reject move
-                if delta <= 0:
+                if self._is_improvement(0, delta):
 
                     # better than the current state --> accept
                     self._solution.move(move)
                     base_value = base_value + delta
 
                     # check if best state
-                    if base_value < self._solution.best_order_value:
+                    if self._is_better(
+                            self._solution.best_order_value, base_value):
                         self._solution.set_as_best(base_value)
 
                 else:
@@ -153,11 +220,12 @@ class SimulatedAnnealing(AbstractLocalSearch):
                     # worse than current state --> use acceptance function.
                     if self._acceptance_function.accept(
                             delta, self._temperature):
-
                         self._solution.move(move)
                         base_value = base_value + delta
 
+                self._termination_criterion.check_new_value(base_value)
                 self._termination_criterion.iteration_done()
+
                 iterations -= 1
 
             # lowers the current temperature
