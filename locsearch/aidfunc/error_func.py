75b83990e8b64d25c1e11b38bfa144840b67d9c8
Daan Thijs <daan.thijs1@student.kuleuven.be>
2019-02-19 15:33:43 +0100

    Added new error func.

--- locsearch/aidfunc/error_func.py
+++ locsearch/aidfunc/error_func.py
@@ -4,7 +4,7 @@ def _not_implemented(*args):
 
     Parameters
     ----------
-    args
+    *args
         Variable length argument list, it does not matter what or how many
         parameters you use. Does not accept keyword arguments.
 
@@ -14,4 +14,30 @@ def _not_implemented(*args):
         This error will always be raised.
 
     """
+
     raise NotImplementedError
+
+
+class WrongMoveType(Exception):
+    """Is raised when the wrong move type is used."""
+    pass
+
+
+def not_multi_move_type(*args):
+    """An error raising function that can take any amount of parameters.
+
+    Parameters
+    ----------
+    *args
+        Variable length argument list, it does not matter what or how many
+        parameters you use. Does not accept keyword arguments.
+
+    Raises
+    ------
+    WrongMoveType
+        This error will always be raised.
+
+    """
+
+    raise WrongMoveType('This method requires you to use an object of the '
+                        'MultiNeighbourhood class as the move_function.')
